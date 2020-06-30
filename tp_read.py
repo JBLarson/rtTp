8e630ebfa27db0783f8983a8f0871dd2aee55438
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-30 14:27:36 -0400

    read data in .txt files, display mpl visual

--- tp_read.py
+++ tp_read.py
@@ -2,91 +2,52 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 
-fig = plt.figure(figsize=(6, 10))
-#ax1 = fig.add_subplot(111)
+from tp_write import b1T, s1T
 
-ax1, ax2 = fig.add_subplot(411), fig.add_subplot(412)
-ax3, ax4 = fig.add_subplot(413), fig.add_subplot(414)
-
+fig = plt.figure(figsize=(10, 6))
+ax1, ax2 = fig.add_subplot(221), fig.add_subplot(222)
+ax3, ax4 = fig.add_subplot(223), fig.add_subplot(224)
 
 def animate(i):
-    pullData = open("rezults.txt","r").read()
-    dataArray = pullData.split('\n')
-    pts, net, b1, s1 = [], [], [], []
-    for eachLine in dataArray:
-        if len(eachLine)>1:
-            x,y = eachLine.split(',')
-            pts.append(float(x))
-            net.append(float(y))
-    
-    pullData1 = open("b1s1.txt", "r").read()
-    dataArray1 = pullData1.split('\n')
-    for eachLine in dataArray1:
-        if len(eachLine)>1:
-            j,b = eachLine.split(',')
-            b1.append(float(j))
-            s1.append(float(b))
+	pullData = open("rezults.txt","r").read()
+	dataArray = pullData.split('\n')
+	pts, net, b1, s1 = [], [], [], []
+	for eachLine in dataArray:
+		if len(eachLine)>1:
+			x,y = eachLine.split(',')
+			pts.append(float(x))
+			net.append(float(y))
+	
+	pullData1 = open("b1s1.txt", "r").read()
+	dataArray1 = pullData1.split('\n')
+	for eachLine in dataArray1:
+		if len(eachLine)>1:
+			j,b = eachLine.split(',')
+			b1.append(float(j))
+			s1.append(float(b))
 
-    ax1.clear()
-    ax1.stackplot(pts,b1)
-    ax1.set_title('Real-Time Total Point Model', fontsize=20)
-    ax1.set_ylabel('Over($)')
-    ax2.clear()
-    ax2.stackplot(pts,s1)
-    ax2.set_ylabel('Under($)')
-    ax3.clear()
-    ax3.stackplot(pts,net)
-    ax3.set_ylabel('Net($)')
-    ax4.clear()
-    ax4.stackplot(pts,b1, labels=['over'])
-    ax4.stackplot(pts,s1, labels=['under'])
-    ax4.stackplot(pts,net, labels=['net'])
-    ax4.set_xlabel('Total Points')
-    ax4.set_ylabel('All($)')
-    plt.legend(loc='lower left')
+	ax1.clear()
+	ax1.stackplot(pts,b1)
+	ax1.set_title(b1T, fontsize=14)
+	ax1.set_ylabel('Result($)', fontsize=14)
+	ax1.set_xticks([])
+	ax2.clear()
+	ax2.stackplot(pts,s1)
+	ax2.set_title(s1T, fontsize=14)
+	ax2.set_xticks([])
+	ax3.clear()
+	ax3.stackplot(pts,net)
+	ax3.set_title('Net Position', fontsize=14)
+	ax3.set_xlabel('Total Points', fontsize=14)
+	ax3.set_ylabel('Result($)', fontsize=14)
+	ax4.clear()
+	ax4.stackplot(pts,b1, labels=['over'])
+	ax4.stackplot(pts,s1, labels=['under'])
+	ax4.stackplot(pts,net, labels=['net'])
+	ax4.set_xlabel('Total Points', fontsize=14)
+	ax4.set_title('All Positions', fontsize=14)
+	plt.legend(loc='lower left')
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 
-
-
-
-
-
-def animat3(i):
-    pullData = open("rezults.txt","r").read()
-    dataArray = pullData.split('\n')
-    pts, net, b1, s1 = [], [], [], []
-    for eachLine in dataArray:
-        if len(eachLine)>1:
-            x,y = eachLine.split(',')
-            pts.append(float(x))
-            net.append(float(y))
-    
-    pullData1 = open("b1s1.txt", "r").read()
-    dataArray1 = pullData1.split('\n')
-    for eachLine in dataArray1:
-        if len(eachLine)>1:
-            j,b = eachLine.split(',')
-            b1.append(float(j))
-            s1.append(float(b))
-
-    ax1.clear()
-    ax1.stackplot(pts,b1, labels=['over'])
-    ax1.stackplot(pts,s1, labels=['under'])
-    ax1.stackplot(pts,net, labels=['net'])
-
-    ax1.set_title('Over, Under, and Net Positions')
-    ax1.set_xlabel('Total Points')
-    ax1.set_ylabel('Result($)')
-    plt.legend(loc='upper left')
-
-
-
-#anim = animation.FuncAnimation(fig, animat3, interval=1000)
-
-
-
 plt.show()
-
-
-
